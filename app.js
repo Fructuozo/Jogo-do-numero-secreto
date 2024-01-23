a1f952c5c57f166bf92c37a0bbf37e7ecda10916
Gabriel Fructuozo <gabrielfruc@gmail.com>
2024-01-23 17:41:33 -0300

    v1

---
+++ app.js
@@ -0,0 +1,7 @@
+ let titulo = document.querySelector('h1');
+ titulo.innerHTML = 'Jogo do número secreto';
+
+ let parafrafo = document.querySelector('p');
+ parafrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+ verificarChute() 
