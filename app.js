4a33abc011595ece310f9c320be51f0be28a6ada
Gabriel Fructuozo <gabrielfruc@gmail.com>
2024-01-23 20:03:48 -0300

    concluido

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
- let titulo = document.querySelector('h1');
- titulo.innerHTML = 'Jogo do número secreto';
+let listaDeNumeroSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
- let parafrafo = document.querySelector('p');
- parafrafo.innerHTML = 'Escolha um número entre 1 e 10';
+ function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+ }
 
- verificarChute() 
+ function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+  
+ function verificarChute() {
+    let chute = document.querySelector('input').value
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+       if (chute > numeroSecreto) {
+           exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+       }
+       tentativas++
+
+        limparCampo()
+    }
+ }
+
+ function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumeroSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumeroSorteados = [];
+    }
+
+    if (listaDeNumeroSorteados.includes (numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    } else { 
+        listaDeNumeroSorteados.push(numeroEscolhido)
+        return numeroEscolhido;
+    }
+ }
+
+ function limparCampo(){
+     exibirMensagemInicial()
+    chute = document.querySelector('input');
+    chute.value = '';
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+ }
+
+
+ function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio()
+    limparCampo()
+    tentativas = 1;
+ }
